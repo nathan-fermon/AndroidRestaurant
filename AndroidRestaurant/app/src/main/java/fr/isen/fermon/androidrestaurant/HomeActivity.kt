f48661bd4f60e1b4be9e1ecd0ee7c984f58a9388
nathan-fermon <64951986+nathan-fermon@users.noreply.github.com>
2021-01-25 11:00:25 +0100

    Initial commit

---
+++ AndroidRestaurant/app/src/main/java/fr/isen/fermon/androidrestaurant/HomeActivity.kt
@@ -0,0 +1,18 @@
+package fr.isen.fermon.androidrestaurant
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+    }
+}
